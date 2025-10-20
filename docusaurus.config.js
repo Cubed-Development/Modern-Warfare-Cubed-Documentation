057ab9b87ef138a39e7711c99b05b23d3a518dbc
Desoroxxx <desoroxxx.git@redstudio.dev>
2025-10-20 13:04:52 -0700

    Update site URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://cubed-development.github.io/',
+  url: 'https://docs.mwc.redstudio.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: 'Modern-Warfare-Cubed-Documentation',
